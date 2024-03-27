4b0076732d40a6cbe868672e0d13eb5b55c56af3
Luke Simmons <luke5083@live.com>
2024-03-27 15:13:05 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    signingConfigs {
+        create("release") {
+        }
+    }
+    namespace = "com.lukesimmons.galleryvision"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.lukesimmons.galleryvision"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+        signingConfig = signingConfigs.getByName("debug")
+        multiDexEnabled = false
+        testFunctionalTest = true
+        testHandleProfiling = true
+        testApplicationId = "com.lukesimmons.galleryvisiontest"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isDebuggable = false
+            isJniDebuggable = false
+            renderscriptOptimLevel = 3
+            signingConfig = signingConfigs.getByName("release")
+            multiDexEnabled = false
+        }
+        getByName("debug") {
+            isDebuggable = true
+            isJniDebuggable = true
+            renderscriptOptimLevel = 0
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "dbg"
+            multiDexEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    dependenciesInfo {
+        includeInApk = true
+        includeInBundle = true
+    }
+    buildToolsVersion = "34.0.0"
+    ndkVersion = "25.1.8937393"
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx")
+    implementation("androidx.activity:activity-compose")
+    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.2")
+}
