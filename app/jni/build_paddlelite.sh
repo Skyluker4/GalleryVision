d9b7c07670603079f5e5f0b9f554ba5304161a46
Luke Simmons <luke5083@live.com>
2024-06-11 18:59:09 -0500

    Load model

--- app/jni/build_paddlelite.sh
+++ app/jni/build_paddlelite.sh
@@ -3,15 +3,15 @@
 set -e
 
 export PATH="/opt/Android/cmake/3.22.1/bin:$PATH"
-export LITE_BUILD_THREADS=$(nproc)
-export NDK_ROOT=/opt/Android/ndk/25.2.9519653
-export NDK_NAME=android-ndk-r25c
+LITE_BUILD_THREADS=$(nproc) || 1
+export LITE_BUILD_THREADS
+export NDK_ROOT="/opt/Android/ndk/25.2.9519653"
+export NDK_NAME="android-ndk-r25c"
 
 # Debug
 #SHARED_FLAGS="--android_stl=c++_shared --with_extra=ON --with_cv=ON --with_log=ON --with_exception=ON --with_nnadapter=ON --nnadapter_with_android_nnapi=ON --android_api_level=33"
 #./Paddle-Lite/lite/tools/build_android.sh  --with_arm82_fp16=ON --with_arm8_sve2=ON "${SHARED_FLAGS}"
 #./Paddle-Lite/lite/tools/build_android.sh --arch=armv7 --toolchain=clang "${SHARED_FLAGS}"
-
 
 # Release
 SHARED_FLAGS="--android_stl=c++_shared --with_extra=ON --with_cv=ON --with_log=OFF --with_exception=OFF --with_nnadapter=ON --nnadapter_with_android_nnapi=ON --android_api_level=33"
--- app/src/main/java/com/lukesimmons/galleryvision/MainActivity.kt
+++ app/src/main/java/com/lukesimmons/galleryvision/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.lukesimmons.galleryvision
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,8 +15,42 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.lukesimmons.galleryvision.ui.theme.GalleryVisionTheme
 
 class MainActivity : ComponentActivity() {
+    protected var modelPath: String = "models/ch_PP-OCRv2"
+    protected var labelPath: String = "labels/ppocr_keys_v1.txt"
+    protected var imagePath: String = "images/det_0.jpg"
+    protected var cpuThreadNum: Int = 1
+    protected var cpuPowerMode: String = "LITE_POWER_HIGH"
+    protected var detLongSize: Int = 960
+    protected var scoreThreshold: Float = 0.1f
+
+    protected var predictor: Predictor = Predictor()
+
+    private var cur_predict_image: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val `in` = assets.open(imagePath)
+        val bmp = BitmapFactory.decodeStream(`in`)
+        cur_predict_image = bmp
+
+        if (predictor.isLoaded()) {
+            predictor.releaseModel()
+        }
+
+        val success = predictor.init(
+            this@MainActivity,
+            modelPath,
+            labelPath,
+            1,
+            cpuThreadNum,
+            cpuPowerMode,
+            detLongSize,
+            scoreThreshold
+        )
+
+        println("Model loaded: $success")
+
         setContent {
             GalleryVisionTheme {
                 // A surface container using the 'background' color from the theme
